c7d7e6102d3c117dcfb4947191776a6d68d072c5
Collisteru <54218252+Collisteru@users.noreply.github.com>
2021-10-01 11:18:44 -0700

    Update Etymology.py
    Added decoder support

--- Etymology.py
+++ Etymology.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen, Request
+from decoder import languageDecoder
 
 def wiktionaryLookup(word):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
@@ -6,12 +7,18 @@ def wiktionaryLookup(word):
         data = urlopen(Request(url="https://en.wiktionary.org/wiki/"+word, headers=headers)).read()
     except:
         print("An error occured.")
-        exit()
+        return 0
     data = data.decode("utf-8","replace")
     pos = data.find("<span class=\"mw-headline\" id=\"Etymology\">")
     etym = data[data.find("<p>", pos)+3:data.find("<h3>", pos)]
     while(etym.find("mention\" lang=")!=-1):
-        print(etym[etym.find("lang=", etym.find("mention\" lang="))+6:etym.find(">", etym.find("mention\" lang="))-1])
+        code = etym[etym.find("lang=", etym.find("mention\" lang="))+6:etym.find(">", etym.find("mention\" lang="))-1]
+        try:
+            language = languageDecoder(code)
+        except:
+            #Code not found in languageDict, consider adding
+            language = code
+        print(language)
         etym = etym[etym.find("mention\" lang=")+1:]
 
 
