878c071c94e0e4cc696ac6db19c02880c8d8fb96
Collisteru <54218252+Collisteru@users.noreply.github.com>
2021-10-09 15:31:01 -0700

    Added Build Source Files

---
+++ src/minTest.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	  render() {	
+		return (
+			<div className="shopping-list">
+				<h1>" Shopping list for someone."  </h1>
+
+
+			</div>
+
+
+
+
+
+		);
+
+
+
+
+	  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
